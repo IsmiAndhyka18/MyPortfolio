1f2b2d1796f9b4810d4e0a82270bf2e9c38ea3e6
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-16 23:58:36 +0200

    adding dowloan cv

--- Script.js
+++ Script.js
@@ -1,5 +1,18 @@
 
-console.log("its work")
-(function(){
-    emailjs.init("");
- })();
+function sendMail(){
+    var tempParams={
+        from_name:document.getElementById("name").value,
+        to_name:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+    };
+      console.log(tempParams);
+    emailjs.send('gmail','template_0qw0fzt',tempParams)
+    .then(function(res){
+       
+     
+
+    })
+    console.log("succes")
+
+    }
+  
