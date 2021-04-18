6ac8a58e5ebd9e041c8b9c61193d1175d0577ff5
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-18 22:34:25 +0200

    adding AOS animation

--- Script.js
+++ Script.js
@@ -1,18 +1,5 @@
-
-function sendMail(){
-    var tempParams={
-        from_name:document.getElementById("name").value,
-        to_name:document.getElementById("email").value,
-        message:document.getElementById("message").value,
-    };
-      console.log(tempParams);
-    emailjs.send('gmail','template_0qw0fzt',tempParams)
-    .then(function(res){
-       
-     
-
-    })
-    console.log("succes")
-
-    }
+AOS.init({
+  offset : 370,
+  duration: 1000
+});
   
